068aab9ca51c8710665a40d8628bb372cd8e585a
muunseo <mkogo90@naver.com>
2025-07-06 15:12:08 +0900

    feat: 커뮤니티 글 등록

--- community/views.py
+++ community/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics, filters
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import PostCreateSerializer, PostSerializer
 
 class PostListView(generics.ListAPIView):
     queryset = Post.objects.all().order_by('-created_at')
@@ -8,3 +13,14 @@ class PostListView(generics.ListAPIView):
 
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']  
+
+class PostCreateAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = PostCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            post = serializer.save(author=request.user)  # author 추가
+            result_serializer = PostSerializer(post)
+            return Response(result_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
