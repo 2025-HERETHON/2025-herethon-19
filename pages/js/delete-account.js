1a02d5a3fab21ec55890a09c3dc119cf6855d79f
chldbsdud <142567232+chldsbdud@users.noreply.github.com>
2025-07-11 14:09:29 +0900

    Revert "[feat] mypage mentee"

--- pages/js/delete-account.js
+++
@@ -1,6 +0,0 @@
-const checkbox = document.getElementById("withdraw-confirm");
-const button = document.getElementById("withdraw-btn");
-
-checkbox.addEventListener("change", function () {
-  button.disabled = !this.checked;
-});
--- point/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import PointHistory
-
-# Register your models here.
-admin.site.register(PointHistory)
